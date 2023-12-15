ef962a9211cee1543e588b17607e1979fbcd1e1d
Aditya Jindal <adityajindal1509@gmail.com>
2023-12-16 01:33:21 +0530

    added quiz instructions

--- src/StartScreen.jsx
+++ src/StartScreen.jsx
@@ -10,7 +10,7 @@ function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <Header />
-      <h2>Mathematical Talent Exam</h2>
+      <h2>Mathematical Talent Exam (DEMO)</h2>
       <form onSubmit={(e) => handleSubmit(e)} className="beginform">
         <div className="forminput">
           <h4>Name:</h4>
@@ -34,6 +34,32 @@ function StartScreen({ numQuestions, dispatch }) {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
+        <h3 className="instructions-heading">Instructions:</h3>
+        <ul>
+          <li>
+            <h5>
+              Use the same Email ID that was used to register for the event, once entered it cannot be changed.
+            </h5>
+          </li>
+          <li>
+            <h5>There will be 5 sections, with 1 question each.</h5>
+          </li>
+          <li>
+            <h5>
+              Navigation between questions is disabled. Answer the questions as
+              you come across them.
+            </h5>
+          </li>
+          <li>
+            <h5>You will have 4 mins to attempt the entire quiz.</h5>
+          </li>
+          <li>
+            <h5>
+              Once you attempt all the questions or the time runs out, the quiz
+              will submit automatically.
+            </h5>
+          </li>
+        </ul>
         <button
           type="submit"
           className="btn  "
